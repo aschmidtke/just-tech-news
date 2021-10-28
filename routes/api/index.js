1f8da8d5dc305edf08f45cf869ee36403e335558
Drew Schmidtke <amschmidtke@gmail.com>
2021-10-27 21:35:40 -0400

    more routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
-
+const Post = require('../../models/Post.js');
+const User = require('../../models/User.js');
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
